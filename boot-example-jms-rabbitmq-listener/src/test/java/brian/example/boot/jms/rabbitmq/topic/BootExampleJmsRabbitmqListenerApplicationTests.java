9dc1abc7aa19e517d714d6d57a03440bd8f72653
Brian Heo <allthatjava@yahoo.ca>
2020-02-09 10:11:51 -0500

    Removes unnecessary tests

--- boot-example-jms-rabbitmq-listener/src/test/java/brian/example/boot/jms/rabbitmq/topic/BootExampleJmsRabbitmqListenerApplicationTests.java
+++ boot-example-jms-rabbitmq-listener/src/test/java/brian/example/boot/jms/rabbitmq/topic/BootExampleJmsRabbitmqListenerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BootExampleJmsRabbitmqListenerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
